32831c32d64df510afe9092642317651404c21eb
Tom Paine <tom.le.paine@gmail.com>
2016-04-22 12:35:50 -0500

    Add funcs and its tests.

---
+++ funcs.py
@@ -0,0 +1,5 @@
+def identity(x):
+    return x
+
+def addition(a, b):
+    return a + b
---
+++ funcs_test.py
@@ -0,0 +1,7 @@
+import funcs
+
+def test_identity():
+    assert funcs.identity(5) == 5
+
+def test_addition():
+    assert funcs.addition(1, 1) == 2
