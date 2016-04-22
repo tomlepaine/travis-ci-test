fec05d9f197ff2d11985938f2279449e209eecd3
Tom Paine <tom.le.paine@gmail.com>
2016-04-22 13:33:24 -0500

    :white_check_mark: Add tf test. Add dependancies.

--- funcs_test.py
+++ funcs_test.py
@@ -19,3 +19,11 @@ def test_import_requests():
 def test_numpy():
     import numpy
     assert type(numpy.array(0)) == numpy.ndarray
+
+
+def test_tf():
+    import tensorflow as tf
+    x = tf.placeholder(tf.float32)
+    sess = tf.Session()
+    out = sess.run(1*x, feed_dict={x: 1})
+    assert out == 1
